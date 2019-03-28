755b9594af2ff1337859f86eb3aea973ced27540
DavidIvanov <41933470+DavidIvanov@users.noreply.github.com>
2019-03-28 15:39:30 -0400

    Add files via upload
    Added support for arrays and get set methods

--- Assignment3.cpp
+++ Assignment3.cpp
@@ -9,34 +9,48 @@ class SmartPointer
 {
 private:
 	T* i;
+	int size;
 public:
 	SmartPointer() {
-		i = new T;
+		size = 1;
+		i = new T[1];
 		(*i) = 0;
 		//cout << "Created " << (*i) << endl;
 	};
 
 	SmartPointer(T j) {
-
+		size = 1;
 		if (sizeof(j) > sizeof(T)) {
 			throw "Error: This is too large.";
 		}
 		if (j < 0) {
 			throw "We don't do negatives.";
 		}
-		i = new T(j);
+		i = new T[1];
+		(*i) = j;
 		//cout << "Created " << (*i) << endl;
 	};
 
+	SmartPointer(T j[], int _size) {
+		size = _size;
+		i = new T[size];
+		for (int k = 0; k < _size; k++) {
+			i[k] = j[k];
+		}
+	}
+
 	~SmartPointer() {
-		delete i;
+		delete[] i;
 		i = NULL;
 		cout << "Deleted" << endl;
 	};
 
+
 	T setValue(T j);
+	T setValue(T j, int index);
 	T getValue();
-	
+	T getValue(int index);
+
 	friend SmartPointer operator+(const SmartPointer<T> &sp1, const SmartPointer<T> &sp2) {
 		SmartPointer<T> result(0);
 		T* p1 = sp1.i;
@@ -51,12 +65,12 @@ public:
 		SmartPointer<T> result(0);
 		T* p1 = sp1.i;
 		T* p2 = sp2.i;
-		T sum;
-		sum = (*p1) - (*p2);
-		if (sum < 0) {
+		T diff;
+		diff = (*p1) - (*p2);
+		if (diff < 0) {
 			throw "We don't do negatives.";
 		}
-		result.setValue(sum);
+		result.setValue(diff);
 		return result;
 	};
 
@@ -79,13 +93,36 @@ T SmartPointer<T>::setValue(T j) {
 	if (j < 0) {
 		throw "We don't do negatives.";
 	}
-	this->i = new T(j);
+	(*i) = j;
 	return 0;
+}
+
+template <class T>
+T SmartPointer<T>::setValue(T j, int index) {
+	if (sizeof(j) > sizeof(T)) {
+		throw "Error: This is too large.";
+	}
+	if (j < 0) {
+		throw "We don't do negatives.";
+	}
+	if (index >= size) {
+		throw "You are out of bounds.";
+	}
+	i[index] = j;
+	return i[index];
 }
 
 template <class T>
 T SmartPointer<T>::getValue() {
 	return (*i);
+}
+
+template <class T>
+T SmartPointer<T>::getValue(int index) {
+	if (index >= size) {//maybe throw exception
+		return 0;
+	}
+	return i[index];
 }
 
 int main() {
@@ -123,7 +160,17 @@ int main() {
 
 	SmartPointer<float> SmartFloatPointer5 = SmartFloatPointer1 * SmartFloatPointer2;
 	cout << "SmartFloatPointer1 * SmartFloatPointer2 = " << SmartFloatPointer5.getValue() << endl;
+	
+	float arr[] = {1, 2, 3, 4, 5, 6, 7};
+	SmartPointer<float> smartArray(arr, 7);
+
+	for (int i = 0; i < 7; i++) {
+		smartArray.setValue((float) i, i);
+		cout << smartArray.getValue(i) << " ";
+	}
+	cout << endl;
 
 	return 0;
+
 
 }
